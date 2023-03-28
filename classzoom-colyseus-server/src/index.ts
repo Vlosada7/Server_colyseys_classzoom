bb0150fd6de5cd3901b698d90d8b7381985e7845
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-28 12:10:15 +0200

    Feat: colyseus server

---
+++ classzoom-colyseus-server/src/index.ts
@@ -0,0 +1,57 @@
+// import Arena from "@colyseus/arena";
+import express from 'express';
+import { monitor } from "@colyseus/monitor";
+import { Server } from 'colyseus';
+import { createServer } from 'http';
+import { WebSocketTransport } from '@colyseus/ws-transport';
+import { MySchool } from "./rooms/MySchool";
+
+const gamePort = 4001;
+
+const app = express();
+
+const server = createServer();
+const gameServer = new Server({
+  transport: new WebSocketTransport({
+    server, // provide the custom server for `WebSocketTransport`
+  }),
+});
+gameServer.define('my_school', MySchool);
+app.use('/colyseus', monitor());
+
+gameServer.listen(gamePort);
+
+// export default Arena({
+//     getId: () => "Your Colyseus App",
+
+//     initializeGameServer: (gameServer) => {
+//         /**
+//          * Define your room handlers:
+//          */
+//         gameServer.define('my_school', MySchool);
+
+//     },
+
+//     initializeExpress: (app) => {
+//         /**
+//          * Bind your custom express routes here:
+//          */
+//         app.get("/", (req, res) => {
+//             res.send("It's time to kick ass and chew bubblegum!");
+//         });
+
+//         /**
+//          * Bind @colyseus/monitor
+//          * It is recommended to protect this route with a password.
+//          * Read more: https://docs.colyseus.io/tools/monitor/
+//          */
+//         app.use("/colyseus", monitor());
+//     },
+
+
+//     beforeListen: () => {
+//         /**
+//          * Before before gameServer.listen() is called.
+//          */
+//     }
+// });
